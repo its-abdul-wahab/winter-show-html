e5401e3b9c687273feae0db7b9e399d0ca28d894
Saqib Syed <sa@techscale.io>
2023-08-12 19:22:26 +0500

    mobile carousel-like animation fixed

--- js/index.js
+++ js/index.js
@@ -67,42 +67,120 @@ function scrollZoom() {
 
 scrollZoom();
 
+let currentSectionIndex = 1;
 
+const indicators = document.querySelectorAll('.indicator');
+const sections = document.querySelectorAll('.site-section');
+
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5, // Adjust this threshold as needed
+  };
+  
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            const sectionID = entry.target.id.split('-')[1]
+            // console.log('In Observer ', index + 1, entry.target.id, Number(sectionID))
+            currentSectionIndex = Number(sectionID)
+            updateActiveIndicator(Number(sectionID));
+        }
+    });
+}, observerOptions);
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
 
 
 function scrollToSection(index) {
     const section = document.getElementById(`section-${index}`)
-    const indicators = document.querySelectorAll('.indicator');
+    // console.log('CURRENT_SECTION_INDEX = ', currentSectionIndex)
+    // console.log('INDEX = ', index)
 
-    section.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    if (section) {
+        // console.log('innerHeight ', window.innerHeight, window.innerHeight * (index - currentSectionIndex))
+        if (currentSectionIndex < index) {
+            window.scrollBy(0, window.innerHeight * (index - currentSectionIndex));
+            currentSectionIndex = index;
+        }
+        else if (currentSectionIndex > index) {
+            window.scrollBy(0, -window.innerHeight * (currentSectionIndex - index));
+            currentSectionIndex = index;
+        }
+        else console.log('nothing match ', index, currentSectionIndex);
+      }
+      
+    //   console.log('updated currentSectionIndex = ', currentSectionIndex)
+}
 
-    updateIndicatorsVisibility(index);
+
+window.addEventListener('scroll', () => {
+    const windowTop = window.scrollY;
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.clientHeight;
+      
+      if (windowTop >= sectionTop && windowTop < sectionBottom) {
+        updateActiveIndicator(index + 1);
+      }
+    });
+  });
+
+
+function updateActiveIndicator(activeIndex) {
+    // console.log('activeIndex ', activeIndex)
+    indicators.forEach((indicator, i) => {
+        if (i === activeIndex) indicator.classList.add('active');
+        else indicator.classList.remove('active');
+      });
+    //   console.log('UPDATING...', activeIndex)
+      
+    for (let i = 0; i < indicators.length; i++) {
+    const indicator = indicators[i];
+
+    // Determine whether to show or hide the indicator based on activeIndicator
+        if (i === activeIndex - 1) {
+            // console.log('first conxxx ', i, activeIndex - 1);
+            indicator.classList.add('active');
+            indicator.style.display = 'block';
+        } else if (i === activeIndex - 2 || i === activeIndex || (activeIndex === 1 && i === activeIndex+1) || (activeIndex == 11 && i === activeIndex-3)) {
+            // console.log('second conxxx ', i, activeIndex - 2);
+            indicator.classList.remove('active');
+            indicator.style.display = 'block';
+        } else {
+            // console.log('elsexxx ', i);
+            indicator.style.display = 'none';
+        }
+    }
 }
 
 // updateIndicatorsVisibility(1);
 
-function updateIndicatorsVisibility(activeIndex) {
-    const indicators = document.querySelectorAll('.indicator');
+// function updateIndicatorsVisibility(activeIndex) {
+//     const indicators = document.querySelectorAll('.indicator');
 
-    console.log('indexx ', activeIndex)
-    for (let i = 0; i < indicators.length; i++) {
-        const indicator = indicators[i];
+//     console.log('updating indexjs ', activeIndex)
+//     for (let i = 0; i < indicators.length; i++) {
+//         const indicator = indicators[i];
     
-        // Determine whether to show or hide the indicator based on activeIndicator
-        if (i === activeIndex - 1) {
-            console.log('first con ', i, activeIndex - 1);
-            indicator.classList.add('active');
-            indicator.style.display = 'block';
-          } else if (i === activeIndex - 2 || i === activeIndex || (activeIndex === 1 && i === activeIndex+1) ||  (activeIndex === 11 && i === activeIndex-3)) {
-            console.log('second con i === activeIndex - 2', i, activeIndex - 2);
-            console.log('second con index === activeIndex', i, activeIndex);
-            console.log('second con activeIndex === 1 and i === activeIndex + 1', i, activeIndex, activeIndex+1);
-            console.log('second con activeIndex === 12 and activeIndex - 3', i, activeIndex - 3);
-            indicator.classList.remove('active');
-            indicator.style.display = 'block';
-          } else {
-            console.log('else ', i);
-            indicator.style.display = 'none';
-          }
-      }
-  }
+//         // Determine whether to show or hide the indicator based on activeIndicator
+//         if (i === activeIndex - 1) {
+//             // console.log('first con ', i, activeIndex - 1);
+//             indicator.classList.add('active');
+//             indicator.style.display = 'block';
+//           } else if (i === activeIndex - 2 || i === activeIndex || (activeIndex === 1 && i === activeIndex+1) ||  (activeIndex === 11 && i === activeIndex-3)) {
+//             // console.log('second con i === activeIndex - 2', i, activeIndex - 2);
+//             // console.log('second con index === activeIndex', i, activeIndex);
+//             // console.log('second con activeIndex === 1 and i === activeIndex + 1', i, activeIndex, activeIndex+1);
+//             // console.log('second con activeIndex === 12 and activeIndex - 3', i, activeIndex - 3);
+//             indicator.classList.remove('active');
+//             indicator.style.display = 'block';
+//           } else {
+//             // console.log('else ', i);
+//             indicator.style.display = 'none';
+//           }
+//       }
+//   }
